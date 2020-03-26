216f267a472b17a2b78b7464161f35295a9eaf6c
dmitriy <diman15.skorobogatow@gmail.com>
2020-03-26 18:38:41 +0700

    3 ver of my project

--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,9 @@
-﻿#include <iostream>
-
+﻿
+#include <iostream>
+using namespace std;
 int main()
 {
-    getchar();
-    std::cout << "Hello World!\n";
+	double a = 5, b = 3;
+	cout << pow(a, b);
+	cin.get();
 }
-
-
